f707c68715ad490e89038e650d68f2cb4bf97f57
Pedro Conrado <conradohpedro@gmail.com>
2025-02-13 09:44:49 -0300

    create home page

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,10 +1,1 @@
-import { Heading, Text } from '@ignite-ui/react'
-
-export default function Home() {
-  return (
-    <>
-      <Heading>Hello</Heading>
-      <Text>Welcome to Ignite UI for Web Components</Text>
-    </>
-  )
-}
+export { default } from './home'
