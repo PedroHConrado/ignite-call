ce1e6709a2dcd6b4a19717bed567a6c0d4ae75f4
Pedro Conrado <conradohpedro@gmail.com>
2025-04-25 15:51:13 -0300

    refactor getWeekDays method

--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -1,4 +1,8 @@
-export function getWeekDays() {
+interface GetWeekDaysParams {
+  short?: boolean
+}
+
+export function getWeekDays({ short = false }: GetWeekDaysParams = {}) {
   const formatter = new Intl.DateTimeFormat('pt-BR', {
     weekday: 'long',
   })
@@ -6,6 +10,10 @@ export function getWeekDays() {
   return Array.from(Array(7).keys()).map((day) => {
     const date = new Date(Date.UTC(2021, 5, day))
     const formattedDay = formatter.format(date)
+
+    if (short) {
+      return formattedDay.substring(0, 3).toUpperCase()
+    }
 
     return formattedDay.charAt(0).toUpperCase() + formattedDay.slice(1)
   })
